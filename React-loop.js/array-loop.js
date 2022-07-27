0efe1652effa6737b6785d8a5125626470ad8430
farhanacsebd <farhana.cse.bd@gmail.com>
2022-07-27 19:43:49 +0600

    loop

---
+++ React-loop.js/array-loop.js
@@ -0,0 +1,12 @@
+numbers=[34,55,77,24,745,123,65,11,14];
+
+// for (var i=0;i<numbers.length;i++){
+//     var number= numbers[i];
+//     console.log(i);
+// }
+
+
+for (var i =0; i<numbers.length; i++){
+    var items=numbers[i];
+    console.log(items);
+}
--- array-loop.js
+++
@@ -1,6 +0,0 @@
-numbers=[34,55,77,24,745,123,65,11,14];
-
-for (var i=0;i<numbers.length;i++){
-    var number= numbers[i];
-    console.log(i);
-}
