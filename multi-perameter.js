7af8e1ae0e9f0bc42dbaee225e3494ed36d32e4f
farhanacsebd <farhana.cse.bd@gmail.com>
2022-07-28 14:55:37 +0600

    function

---
+++ multi-perameter.js
@@ -0,0 +1,14 @@
+function numbers(num1,num2){
+    
+    console.log(num1,num2);
+
+    var plustotal=num1+num2;
+    
+    console.log(plustotal);
+    
+    return 0;
+
+}
+var first=5;
+var second=4;
+var reslt= numbers(first,second)
