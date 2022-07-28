316d2b6787d81e966a1e2b70ae5b9825dafc11c7
farhanacsebd <farhana.cse.bd@gmail.com>
2022-07-28 23:15:54 +0600

    while break

---
+++ break-continue.js
@@ -0,0 +1,27 @@
+// var i = 0;
+
+// while(i<10){
+//     console.log(i);
+//     if(i==5){
+//         break;
+//     }
+//     i++;
+// }
+
+
+var array=[43,5,22,55,77,752,245,756,1223,867,11,33];
+
+var i=0;
+while(i<array.length){
+    
+    // console.log(i);
+
+    var number = array[i]
+    console.log(number);
+
+    if(i>90){
+        break;
+    }
+
+    i++;
+}
