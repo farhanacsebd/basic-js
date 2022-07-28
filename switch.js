c87de4e869e7bdca7e825a2fcfa17b02a6094e75
farhanacsebd <farhana.cse.bd@gmail.com>
2022-07-28 22:46:39 +0600

    switch js

---
+++ switch.js
@@ -0,0 +1,24 @@
+var fruts ='Yellow';
+
+switch(fruts){
+    case 'Orange':
+        console.log('orange are 50kg');
+
+        break;
+
+    case 'Red':
+        console.log('Apple are 80kg');
+        
+        break;
+    
+    case 'Pink':
+        console.log('Dragon are 400kg');
+
+        break;
+
+    case 'Yollow':
+        console.log('Lemon are 29kh');
+
+    default:
+        console.log('this is another fruts');    
+    }
