1780045e3b686dcfbca67f2f2cf648a2ba8b9f69
farhanacsebd <farhana.cse.bd@gmail.com>
2022-07-28 17:34:56 +0600

    tour code

--- perameter.js
+++ perameter.js
@@ -1,11 +1,30 @@
-function bringBarger(taka){
-    // console.log('tk diche ammu');
-    console.log('aj mon vore barger khabo');
-    var bargerPrice = 200;
-    var totalQuantity = taka / bargerPrice;
-    return totalQuantity;
+// function bringBarger(taka){
+//     // console.log('tk diche ammu');
+//     console.log('aj mon vore barger khabo');
+//     var bargerPrice = 200;
+//     var totalQuantity = taka / bargerPrice;
+//     return totalQuantity;
+// }
+
+// var amount = 1000;
+// var totalAmount=bringBarger(amount);
+// console.log('totalBarger =',totalAmount);
+
+
+
+function letsTour(group){
+
+    console.log('cholo sobai ghurte jai');
+
+    var perPersonAmount = 2000;
+
+    var totalAmount = perPersonAmount * group;
+    
+    return totalAmount;
+
 }
 
-var amount = 1000;
-var totalAmount=bringBarger(amount);
-console.log('totalBarger =',totalAmount);
+    var person = 20;
+    var allBudget = letsTour(person)
+
+    console.log('beriye pori',allBudget);
