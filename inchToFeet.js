c02659578aa49a6f4c6e0c1ed153414895bd625e
farhanacsebd <farhana.cse.bd@gmail.com>
2022-07-29 18:44:07 +0600

    inchFeel or mileKelo

---
+++ inchToFeet.js
@@ -0,0 +1,33 @@
+//inchi to feet
+
+function inchToFeet(inches){
+
+   let feet = inches / 12;
+
+   console.log('convert to feet',feet);
+
+   return 0;
+
+}
+
+
+    let toFeet = inchToFeet(158) 
+
+
+    // mile to kelometer
+
+
+    
+function mileToKelometer(kelometer){
+
+   let mile = kelometer/1000;
+
+     console.log(mile);
+
+     return 0;
+
+} 
+
+    let totalkelo = 2000000;
+    
+    let mile=mileToKelometer(totalkelo)
