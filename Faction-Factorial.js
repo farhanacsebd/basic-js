31e609f929602df20c85eb235a62cc70956fa638
farhanacsebd <farhana.cse.bd@gmail.com>
2022-07-29 23:33:27 +0600

    function-factorial

--- Faction-Factorial.js
+++ Faction-Factorial.js
@@ -23,3 +23,42 @@ function letfactorial(number){
 
 var valuofthis=letfactorial(7)
 console.log("This is the factorial number:",valuofthis);
+
+
+
+function factorialNumber(value){
+    var getfactorial =1;
+    var i = 1;
+    while(i<=value){
+        getfactorial =getfactorial*i;
+        i++;
+        
+    }
+    
+    return getfactorial;
+}
+
+var thevalue=7;
+var thisvalue=factorialNumber(thevalue);
+console.log(thisvalue);
+
+
+
+
+
+function factorialNumber(){
+    var getfactorial =1;
+    var i = 1;
+    while(i<=7){
+        getfactorial =getfactorial*i;
+        i++;
+        
+    }
+    console.log(thisvalue);
+    return getfactorial;
+}
+
+
+var thisvalue=factorialNumber(thevalue);
+
+
