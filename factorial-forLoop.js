36280a15d0f763f9619a98862c5ee4834bb250fd
farhanacsebd <farhana.cse.bd@gmail.com>
2022-07-29 22:42:30 +0600

    for loop factorial

---
+++ factorial-forLoop.js
@@ -0,0 +1,17 @@
+var factorial =1;
+
+for(var i=1;i <= 7;i++){
+    
+    var factorial = factorial * i;
+   
+}
+
+console.log(factorial);
+
+
+var fact =1;
+for (var i=1;i<=7;i++){
+   var fact =fact * 1;
+}
+
+console.log(fact);
