e8f3ec1d030cabd045fa023eaa931b63c958e181
farhanacsebd <farhana.cse.bd@gmail.com>
2022-07-30 22:43:06 +0600

    added odd loop

--- practice.js
+++ practice.js
@@ -1,14 +1,14 @@
 const x=30;
 const y=20;
 const z=x+y;
-console.log(z);
+// console.log(z);
 
 
 // while loop
 
 var i=1;
 while(i<=100){
-    console.log(i);
+    // console.log(i);
     i++;
 }
 
@@ -16,5 +16,16 @@ while(i<=100){
 // for loop
 
 for(let i=1; i<=100;i++){
+    // console.log(i);
+}
+
+
+for(var i=51;i<=80;i+=2){
+    // console.log(i);
+}
+
+var i=51;
+while(i<=80){
     console.log(i);
+    i+=2
 }
