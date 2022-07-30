78c0d96ef7b256e1ad42a65668a3096341dcf583
farhanacsebd <farhana.cse.bd@gmail.com>
2022-07-30 22:36:48 +0600

    for/while loop

--- practice.js
+++ practice.js
@@ -2,3 +2,19 @@ const x=30;
 const y=20;
 const z=x+y;
 console.log(z);
+
+
+// while loop
+
+var i=1;
+while(i<=100){
+    console.log(i);
+    i++;
+}
+
+
+// for loop
+
+for(let i=1; i<=100;i++){
+    console.log(i);
+}
