206049b0fcc5f746fe8cb5a5ee6b87f5999fc86d
farhanacsebd <farhana.cse.bd@gmail.com>
2022-07-30 23:21:24 +0600

    weather function

--- practice.js
+++ practice.js
@@ -24,12 +24,19 @@ for(var i=51;i<=80;i+=2){
     // console.log(i);
 }
 
+
+
+
+// while loop
+
 var i=51;
 while(i<=80){
     // console.log(i);
     i+=2
 }
 
+
+// function
 
 function numbers(num1,num2,num3){
     let valueIs =num1+num2+num3;
@@ -42,4 +49,30 @@ let third=30;
 
 let resultIs=numbers(10,20,30)
 
-console.log(resultIs);
+// console.log(resultIs);
+
+
+
+// celcius to Fahrenheit
+
+
+// function celciusToFahrenheit(celcius){
+//     let fahrenheitIs=celcius * 33.8;
+//     return fahrenheitIs;
+// }
+
+//   let celciusIs=30;
+//   let weatherIs = celciusToFahrenheit(celciusIs);
+
+//   console.log('today's fahrenheit:',weatherIs);
+
+
+
+  function fahrenheitToCelcius(fahrenheit){
+        celciusIss=fahrenheit % 33.8;
+        return celciusIss;
+  }
+
+        let fahrenheitToday =1013.9;
+        let weather=fahrenheitToCelcius(fahrenheitToday);
+        console.log('today weath is ',weather);
