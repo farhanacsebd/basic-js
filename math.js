33966aec330861c2ea169b4d47d3ccfa3f90473a
farhanacsebd <farhana.cse.bd@gmail.com>
2022-07-30 22:28:21 +0600

    math js

---
+++ math.js
@@ -0,0 +1,36 @@
+// var myNumber =6;
+// var output = Math.abs(myNumber);
+// console.log(output);
+
+// var herNumber =-7;
+
+// var valuIs=Math.abs(herNumber);
+// console.log(valuIs);
+
+
+
+// var number = 21.44;
+// var theNumber=Math.ceil(number);
+// console.log(theNumber);
+
+
+// var thisnumber = 3.32;
+// var valuIs = Math.ceil(thisnumber);
+// console.log(valuIs);
+
+
+
+var thisnumber = 3.32;
+var valuIs = Math.floor(thisnumber);
+console.log(valuIs);
+
+
+var num = 4.5;
+var numof=Math.round(num);
+console.log(numof);
+
+
+var numberof =3;
+numberofvalue=Math.random()*10;
+console.log(numberofvalue);
+
