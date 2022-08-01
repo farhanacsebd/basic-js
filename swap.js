166d9b5e1e59d000a6235807662ff3d8a841ffca
farhanacsebd <farhana.cse.bd@gmail.com>
2022-08-01 22:25:29 +0600

    destructuring

---
+++ swap.js
@@ -0,0 +1,17 @@
+let first=5;
+let second=77;
+// console.log(first,second);
+
+// let temp = first;
+// first=second;
+// second=temp;
+// console.log(first,second);
+
+
+// destructuring
+
+
+[first,second]=[second,first];
+console.log(first,second);
+
+
