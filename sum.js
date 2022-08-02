7da1b0a4bd748bf052b49d1fd69863cd76463095
farhanacsebd <farhana.cse.bd@gmail.com>
2022-08-03 00:25:09 +0600

    sum number array function

--- sum.js
+++ sum.js
@@ -1,10 +1,30 @@
-let numbers =[33,56,87,93,04,67,22,19];
+// let numbers =[33,56,87,93,04,67,22,19];
 
-let sum =0;
+// let sum =0;
 
-for(let i=0;i<numbers.length;i++){
+// for(let i=0;i<numbers.length;i++){
 
-    element = numbers[i];
-    sum=sum + element;
-    console.log(sum);
+//     element = numbers[i];
+//     sum=sum + element;
+//     console.log(sum);
+// }
+
+
+
+
+
+
+
+
+function array(numbers){
+    let sum =0;
+    for(i=0;i<numbers.length;i++){
+       let element = numbers[i];
+        sum=sum + element;
+    }
+    
+        return sum;
 }
+
+let total=array([33,223,555,665,123,54,23]);
+console.log(total);
