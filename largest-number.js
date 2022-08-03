3bdda7a77d04fb2064342c0688e941ce9b92937c
farhanacsebd <farhana.cse.bd@gmail.com>
2022-08-03 22:39:21 +0600

    largest number

--- largest-number.js
+++ largest-number.js
@@ -1,14 +1,55 @@
-function largerElement(number){
-    let larger = 0;
-    for(let i=0;i<number.length;i++){
-        let element =number[i];
-      if (element > larger){
-        larger = element;
-      }
+// function largerElement(number){
+//     let larger = 0;
+//     for(let i=0;i<number.length;i++){
+//         let element =number[i];
+//       if (element > larger){
+//         larger = element;
+//       }
 
+//     }
+//     return larger;
+// }
+
+
+// console.log(largerElement([43,24,65,86,99,78,34,32]));
+
+
+
+// function largeNumber(number){
+//     let largest = number[0];
+
+//     for(let i=0 ;i<number.length ;i++){
+
+//         let element = number[i];
+
+//         if(element>largest){
+//             largest = element;
+//         }
+//     }
+//     return largest;
+// }
+
+// oldest =[32,23,54,76,90,04,17,77];
+// older=largeNumber(oldest);
+// // console.log(older);
+
+
+
+function biggerNumber(numbers){
+    let i = 0;
+    let largest=number[0];
+    while( i<numbers.length ){
+        
+        element = numbers[i];
+        {
+            if(element>largest){
+                largest = element;
+            }
+        }
+        i++;
     }
-    return larger;
+    return largest;
 }
 
 
-console.log(largerElement([43,24,65,86,99,78,34,32]));
+console.log(biggerNumber[32,23,54,76,90,04,17,77]);
