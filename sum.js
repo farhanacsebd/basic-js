02cba5185f9db169a521e6ad1be0b9e54e6485a2
farhanacsebd <farhana.cse.bd@gmail.com>
2022-08-03 21:25:28 +0600

    function for loop

--- sum.js
+++ sum.js
@@ -16,15 +16,42 @@
 
 
 
-function array(numbers){
-    let sum =0;
-    for(i=0;i<numbers.length;i++){
-       let element = numbers[i];
-        sum=sum + element;
-    }
+// function array(numbers){
+//     let sum =0;
+//     for(i=0;i<numbers.length;i++){
+//        let element = numbers[i];
+//         sum=sum + element;
+//     }
     
-        return sum;
+//         return sum;
+// }
+
+// let total=array([33,223,555,665,123,54,23]);
+// console.log(total);
+
+
+
+ let number =[1,2,4,32,64,67,89,99,43,22,2];
+    let sum=0;
+for(let i=0;i<number.length;i++){
+    // console.log(i);
+    let element=number[i];
+    sum = sum+element;
+    console.log(sum);
 }
 
-let total=array([33,223,555,665,123,54,23]);
-console.log(total);
+
+
+function arr(numbers){
+
+    let sum=0;
+
+    for(let i=0;i<numbers.length;i++){
+        let elements=numbers[i];
+        sum=sum + elements;
+    }
+    return sum;
+}
+
+// console.log(arr([1,2,4,32,64,67,89,99,43,22,2]));
+
