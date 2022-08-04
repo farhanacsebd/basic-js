0daec09d37f4fa96661aa7e5d1edad9c00a0ddbe
farhanacsebd <farhana.cse.bd@gmail.com>
2022-08-04 23:58:11 +0600

    sum for loop

--- recap1.js
+++ recap1.js
@@ -45,3 +45,15 @@ console.log(first,second);
 
 
 
+// sum for loop
+
+
+let numbers =[33,56,87,93,04,67,22,19];
+
+
+sum = 0;
+for(let i=0 ;i<numbers.length; i++){
+    element = numbers[i];
+    sum =sum + element;
+    console.log(sum);
+}
