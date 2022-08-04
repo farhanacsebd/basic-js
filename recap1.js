bdead0c78f9ebd81c9ac5c43d00f10b7e55c65ad
farhanacsebd <farhana.cse.bd@gmail.com>
2022-08-04 23:51:54 +0600

    swap

--- recap1.js
+++ recap1.js
@@ -26,7 +26,22 @@ const rounded =Math.round(resultRandom);
 console.log(rounded);
 
 
+//swap
 
+
+let first = 5;
+let second = 7;
+
+// let temp = first;
+// first = second;
+// second = temp;
+
+// console.log(first,second);
+
+
+
+[first,second] = [second,first];
+console.log(first,second);
 
 
 
