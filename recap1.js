4f66e414815e25c02c5b0945f8e8452013a3d59b
farhanacsebd <farhana.cse.bd@gmail.com>
2022-08-04 23:42:13 +0600

    math solution

---
+++ recap1.js
@@ -0,0 +1,29 @@
+// Math.abs
+
+let mathabs=-5;
+let mathIs=Math.abs(mathabs);
+console.log(mathIs);
+
+// Math.floor
+
+const floor = 34.7;
+const resultFloor = Math.floor(floor);
+console.log(resultFloor);
+
+// Math.ceil
+
+
+const ceilIs = 54.44;
+const resultceil = Math.ceil(ceilIs);
+console.log(resultceil);
+
+
+// Math.random
+
+
+
+// Math.round
+
+
+
+
