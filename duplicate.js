a46e1999957e908e04956b9f546c816bb087765e
farhanacsebd <farhana.cse.bd@gmail.com>
2022-08-05 23:43:39 +0600

    remove duplicate

---
+++ duplicate.js
@@ -0,0 +1,17 @@
+const name =['abul','babul','sabul','dabul','jabul','gabul','abul','babul','sabul','dabul','jabul']
+
+
+function removeDuplicate(name){
+
+    unique=[];
+
+    for(element of name){
+        // console.log(element);
+        if(unique.indexOf(element)==-1){
+            unique.push(element);
+        }
+    }
+    return unique;
+}
+
+console.log(removeDuplicate(name));
