c0c4f988a51a762fb944681d05bcbb60f45fb624
farhanacsebd <farhana.cse.bd@gmail.com>
2022-08-06 22:51:01 +0600

    for loop while loop

--- recap1.js
+++ recap1.js
@@ -39,30 +39,88 @@ console.log(arr);
 console.log(arrlistItem);
 
 
-function result(marks){
-    if(marks>=80 && marks<=100){
-        console.log('Congratulations,You got A+');
-    }
-    else if(marks>=70 && marks<=79){
-        console.log('You got A');
-    }
-    else if(marks>=60 && marks<=69){
-        console.log('Your got A-');
-    }
-    else if(marks>=55 && marks<=59){
-        console.log('Your got B+');
-    }
-    else if(marks>=50 && marks<=54){
-        console.log('Your got B-');
-    }
-    else {
-        console.log('You are fail');
-    }
+// function result(marks){
+//     if(marks>=80 && marks<=100){
+//         console.log('Congratulations,You got A+');
+//     }
+//     else if(marks>=70 && marks<=79){
+//         console.log('You got A');
+//     }
+//     else if(marks>=60 && marks<=69){
+//         console.log('Your got A-');
+//     }
+//     else if(marks>=55 && marks<=59){
+//         console.log('Your got B+');
+//     }
+//     else if(marks>=50 && marks<=54){
+//         console.log('Your got B-');
+//     }
+//     else {
+//         console.log('You are fail');
+//     }
    
+// }
+
+
+// let marksIs=77;
+// totalmarks=result(marksIs);
+
+
+// miles to kilometer
+
+
+function miletokilomer(mile){
+    
+    let km=mile*1000;
+    return km;
+
+}
+
+let mile =10;
+// console.log(miletokilomer(mile));
+
+
+
+
+// inch to feet
+
+function inchtofeet(inches){
+    let feet =inches / 12;
+    return feet;
+
+}
+
+    let inchesIs = 140;
+    // console.log(inchtofeet(inchesIs));
+
+
+
+
+
+let barger  = 550;
+let pizza = 980;
+
+mybudget = 1000;
+
+let haizine = false;
+
+if(mybudget > pizza){
+    if(haizine==true){
+        console.log("I can buy this food");
+    }
+    else{
+        console.log("I can't to buy this.");
+    }
 }
 
 
-let marksIs=77;
-totalmarks=result(marksIs);
+
+// for loop
 
 
+let number=[12,64,77,88,95,32,08,66,79,82,38,21];
+
+for(let i=0;i<number.length;i++){
+   let element=number[i];
+    console.log(element);
+}
