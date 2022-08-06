406594b986371a6876ddc56dfd1929a0e799c893
farhanacsebd <farhana.cse.bd@gmail.com>
2022-08-06 23:39:48 +0600

    factorial for or while

--- recap1.js
+++ recap1.js
@@ -173,7 +173,31 @@ function myNumber(thenumber){
     }
 }
  let theNumberIs = 1221;
- console.log(myNumber(theNumberIs));
+//  console.log(myNumber(theNumberIs));
+
+
+
+// Factorial number for loop
+
+
+
+let factorial =1;
+for(let i=1;i<7;i++){
+    factorial = factorial * i;
+}
+    console.log(factorial);
+
+
+// Factorial number while loop
+
+
+let fact =1;
+let i=1;
+while(i<7){
+    fact = fact * i;
+    i++;
+}
+    console.log(fact);
 
 
 
