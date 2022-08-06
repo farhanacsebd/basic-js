ea3f219fbc0b6377e94c8219a8d555e4a69b2ec2
farhanacsebd <farhana.cse.bd@gmail.com>
2022-08-06 13:55:53 +0600

    recap 17 or 18

--- recap1.js
+++ recap1.js
@@ -1,73 +1,42 @@
-// Math.abs
+// variable
+let first =2;
+let second =4;
 
-let mathabs=-5;
-let mathIs=Math.abs(mathabs);
-console.log(mathIs);
+let total =second / first;
 
-// Math.floor
+console.log(typeof first);
 
-const floor = 34.7;
-const resultFloor = Math.floor(floor);
-console.log(resultFloor);
+ total = true;
 
-// Math.ceil
+console.log(typeof total);
 
 
-const ceilIs = 54.44;
-const resultceil = Math.ceil(ceilIs);
-console.log(resultceil);
-
-
-// Math.random
-// Math.round
-const randomIs = 54.23;
-const resultRandom = Math.random(randomIs)*100;
-const rounded =Math.round(resultRandom);
-console.log(rounded);
-
-
-//swap
-
-
-let first = 5;
-let second = 7;
-
-// let temp = first;
-// first = second;
-// second = temp;
-
-// console.log(first,second);
-
-
-
-[first,second] = [second,first];
-console.log(first,second);
-
-
-
-// sum for loop
-
-
-let numbers =[33,56,87,93,04,67,22,19];
-
-
-sum = 0;
-for(let i=0 ;i<numbers.length; i++){
-    element = numbers[i];
-    sum =sum + element;
-    console.log(sum);
-}
+let nameIs = 'Farhana';
+console.log(typeof nameIs);
 
 
 
 
-function sumarray(number){
-    sum = 0;
-for(let i=0 ;i<number.length; i++){
-    element = number[i];
-    sum =sum + element;
-    console.log(sum);
-}
-}
 
-sumarray([33,56,87,93,04,67,22,19])
+let arr =[22,33,55,234,74,78,99,13,18,67,9];
+
+let arrIs =arr.length;
+
+let arrIndex = arr.indexOf(44);
+
+arr[3] = 77;
+arr.push(21);
+arr.push(90);
+
+// arr.pop();
+
+arr.shift();
+arr.unshift(45);
+
+let arrlistItem = arr.pop();
+
+console.log(arr);
+console.log(arrlistItem);
+
+
+
