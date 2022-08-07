ce1caaa8501e342f2e4be591f5bd69d358464ce6
farhanacsebd <farhana.cse.bd@gmail.com>
2022-08-07 23:59:11 +0600

    foo bar foobar

--- recap1.js
+++ recap1.js
@@ -397,3 +397,23 @@ function fibo(number){
 
  let fiboIs = fibo(10);
  console.log(fiboIs);
+
+
+
+//  foo,bar,foobar
+
+
+for(let i =1;i<=50;i++){
+    if(i % 3 ==0 && i % 5 == 0){
+        console.log('foobar');
+    }
+    if(i % 3 ==0 ){
+        console.log('foo');
+    }
+    if(i % 5 ==0 ){
+        console.log('bar');
+    }
+    else{
+        console.log(i);
+    }
+}
