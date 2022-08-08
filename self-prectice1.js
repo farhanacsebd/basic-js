482e776c984f71da10f9e38aef5e5bae395d3c96
farhanacsebd <farhana.cse.bd@gmail.com>
2022-08-08 22:56:45 +0600

    variable math

--- self-prectice1.js
+++ self-prectice1.js
@@ -1,4 +1,4 @@
-let specialName = 'taSMI';
+/* let specialName = 'taSMI';
 console.log(typeof specialName);
 
 
@@ -8,3 +8,19 @@ console.log(typeof specialNumber);
 
 const theNumber = true;
 console.log(typeof theNumber);
+ */
+
+
+
+const first = 10;
+const second = 5;
+const totalsum = first + second;
+const totalMinus = first - second;
+const totalMultiply = first * second;
+const totaldevide = first % second;
+const totalquotient = first / second;
+console.log(totalsum);
+console.log(totalMinus);
+console.log(totalMinus);
+console.log(totaldevide);
+console.log(totalquotient);
