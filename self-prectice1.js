4af93a118720f70b2a00d8efbfd863dd8c15d6d8
farhanacsebd <farhana.cse.bd@gmail.com>
2022-08-08 23:15:28 +0600

    chick condition

--- self-prectice1.js
+++ self-prectice1.js
@@ -19,8 +19,25 @@ const totalMinus = first - second;
 const totalMultiply = first * second;
 const totaldevide = first % second;
 const totalquotient = first / second;
-console.log(totalsum);
+/* console.log(totalsum);
 console.log(totalMinus);
 console.log(totalMinus);
 console.log(totaldevide);
-console.log(totalquotient);
+console.log(totalquotient); */
+
+
+const phnPrice = 22000;
+const laptopPrice = 50000;
+const buttonPhnPrice = 100;
+
+const myBudget = 22000;
+
+if(myBudget>=phnPrice && myBudget>=laptopPrice){
+    console.log("U take both of these...");
+}
+else if(myBudget>=laptopPrice || myBudget>=phnPrice){
+    console.log('u can take only one');
+}
+else{
+    console.log('buy only buttonphone');
+}
