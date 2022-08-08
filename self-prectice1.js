0c663540fc48c872c368a230532ba04abae7377f
farhanacsebd <farhana.cse.bd@gmail.com>
2022-08-08 20:20:41 +0600

    added readme file

---
+++ self-prectice1.js
@@ -0,0 +1,10 @@
+let specialName = 'taSMI';
+console.log(typeof specialName);
+
+
+let specialNumber = 431;
+console.log(typeof specialNumber);
+
+
+const theNumber = true;
+console.log(typeof theNumber);
