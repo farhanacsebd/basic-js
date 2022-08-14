7c658a80f2ebb463e53be48b1ee59789e10f64a8
farhanacsebd <farhana.cse.bd@gmail.com>
2022-08-15 03:11:00 +0600

    problem 3 done

--- given-problem/test.js
+++ given-problem/test.js
@@ -7,6 +7,14 @@ for(let i = 1;i<10;i+=2){
 // problem no. 2
 
 for(let i = 10;i<=20;i+=2){
-    console.log(i);
+    // console.log(i);
 }
 
+// problem no. 3
+
+let sum =0;
+for(let i = 10;i<=20;i+=2){
+   sum = sum + i;
+   console.log(sum);
+    
+}
